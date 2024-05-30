925ae3b76f449963c35da8cde3680da98a0be871
Divas209 <116635087+Divas209@users.noreply.github.com>
2024-05-30 19:12:10 +0300

    0.0.3

--- index.php
+++ index.php
@@ -2,10 +2,18 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
     <title>Document</title>
-    ds
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <form action="">
+        логин
+        <input type="text">
+        пароль
+        <input type="text">
+        <input type="button" value="войти">
+    </form>
     
 </body>
 </html>
