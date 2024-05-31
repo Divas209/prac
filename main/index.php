aaf8c51e86b1563665d6c04f2a96546a91fe7cd5
Divas209 <116635087+Divas209@users.noreply.github.com>
2024-05-31 17:07:53 +0300

    0.0.4

---
+++ main/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class = "cont_left">
+        <div class ="con1">d</div>
+    </div>
+    <div class = "cont_right">
+        <div class = "con2">d</div>
+        <div class = "con3">d</div>
+    </div>
+</body>
+</html>
